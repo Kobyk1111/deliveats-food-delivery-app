cab3f3a6501b5be7959abe20ea9083df4dd8058e
Worlanyo Kwabla Kporfeame <kworlanyo@gmail.com>
2024-07-03 14:00:18 +0200

    login, register and search connected to backend

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,13 @@ const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/users", userRouter);
 app.use("/search", searchRouter);
