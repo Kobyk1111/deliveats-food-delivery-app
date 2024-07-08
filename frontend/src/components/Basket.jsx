4523890dfaf06691763a596fd9c29609484f99da
Poliana Schuelter <polianaschuelter@polianas-mbp.speedport.ip>
2024-07-08 11:48:40 +0200

    basktet in style in the side

--- frontend/src/components/Basket.jsx
+++ frontend/src/components/Basket.jsx
@@ -1,40 +1,5 @@
-// import { useContext } from "react";
-// import { BasketContext } from "../contexts/BasketContext";
-
-// function Basket() {
-//   const { basket, removeItemFromBasket, totalSum } = useContext(BasketContext);
-
-//   return (
-//     <div className="basket">
-//       <h2>Basket</h2>
-//       {basket.length === 0 ? (
-//         <p>No items in the basket</p>
-//       ) : (
-//         <ul>
-//           {basket.map((item) => (
-//             <li key={item.id}>
-//               <span>{item.name}</span>
-//               <span> ${item.price}</span>
-//               <span> (x{item.quantity})</span>
-//               <span>{item.price * item.quantity}</span>
-
-//               <button onClick={() => removeItemFromBasket(item.id)}>
-//                 Remove
-//               </button>
-//             </li>
-//           ))}
-//         </ul>
-//       )}
-//       <h3>Total: ${totalSum.toFixed(2)}</h3>
-//     </div>
-//   );
-// }
-
-// export default Basket;
-
 import { useContext, useState } from "react";
 import { BasketContext } from "../contexts/BasketContext";
-// import "./Basket.css"; // Make sure you import the CSS file
 
 function Basket() {
   const {
