9bd376bc1e7ac849a3b6dd575fdd3ccc06f6ba52
TomislavMiljak <tomislavmiljak@yahoo.com>
2024-07-16 11:16:10 +0200

    commit at 11.16

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { registerUser, loginUser } from "../controllers/userController.js";
+import { registerUser, loginUser, updateUser, deleteUser, getUserData } from "../controllers/userController.js";
 
 const router = Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.put("/update/:userId", updateUser);
+router.delete("/delete/:userId", deleteUser);
+router.get("/getData/:userId", getUserData);
 
 export default router;
