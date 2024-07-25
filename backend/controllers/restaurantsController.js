7d0151065207f92cf5ed45d900dc87d3f79346a1
Worlanyo Kwabla Kporfeame <kworlanyo@gmail.com>
2024-07-25 11:49:19 +0200

    initial commit

--- backend/controllers/restaurantsController.js
+++ backend/controllers/restaurantsController.js
@@ -5,69 +5,6 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import SearchedRestaurant from "../models/SearchedRestaurantsModel.js";
 
-// export async function getAllRestaurants(req, res, next) {
-//   const { search } = req.body;
-
-//   // const search = "Brazilian food in leipzig";
-
-//   const splitSearch = search.toLowerCase().split(" ");
-
-//   const filteredArray = menus.filter((menu) => splitSearch.includes(menu.cuisine.toLowerCase()));
-
-//   function getRandomMenu() {
-//     const randomIndex = Math.floor(Math.random() * filteredArray.length);
-//     return filteredArray[randomIndex];
-//   }
-
-//   const API_KEY = process.env.GOOGLE_API_KEY;
-
-//   try {
-//     const response = await fetch(
-//       `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${search}&type=restaurant&key=${API_KEY}`
-//     );
-
-//     if (response.ok) {
-//       const data = await response.json();
-
-//       const restaurantsData = data.results.map((restaurant) => {
-//         return {
-//           name: restaurant.name,
-//           address: restaurant.formatted_address,
-//           rating: restaurant.rating,
-//           userRatings: restaurant.user_ratings_total,
-//           isOpen: restaurant.opening_hours ? restaurant.opening_hours.open_now : false,
-//           price_level: restaurant.price_level || 0,
-//           favorited: false,
-//           menu: getRandomMenu(),
-//         };
-//       });
-
-//       try {
-//         await Restaurant.deleteMany({});
-//         const allRestaurants = await Restaurant.create(restaurantsData);
-//         res.json(allRestaurants);
-//       } catch (error) {
-//         return next(createHttpError(400, "Restaurants could not be created"));
-//       }
-//     } else {
-//       const error = await response.json();
-//       return next(createHttpError(400, error.error_message || "Restaurants could not be fetched"));
-//     }
-//   } catch (error) {
-//     next(createHttpError(500, "Please search for cuisines or restaurants in a city"));
-//   }
-// }
-
-// export async function getAllRestaurants(req, res, next) {
-//    const { search } = req.body;
-
-//    const splitSearch = search.toLowerCase().split(" ");
-
-//    if (!splitSearch.includes("Leipzig") || !splitSearch.includes("Berlin") || !splitSearch.includes("Hannover") || !splitSearch.includes("Düsseldorf")) {
-//     return next(createHttpError(400, "Search city should be Leipzig, Berlin, Hannover or Düsseldorf"));
-//    }
-// }
-
 export async function getAllRestaurants(req, res, next) {
   try {
     const { search } = req.body;
@@ -75,6 +12,8 @@ export async function getAllRestaurants(req, res, next) {
     if (!search) {
       return next(createHttpError(400, "Search term is required"));
     }
+
+    console.log(search);
 
     const splitSearch = search.toLowerCase().split(" ");
     const country = splitSearch[0];
